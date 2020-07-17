10e533b3fac37f465f904bde9d0891dec5265dbf
ReddLin <s9016030303@gmail.com>
2020-07-17 15:51:16 +0800

    add mobile icon bar

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,24 +1,39 @@
 <template>
   <div class="header">
-    <div class="header-menu-flex">
-      <h1 class="header-logo">
-        <a @click="goToSomewhoere('')">Redd Lin</a>
-      </h1>
-    </div>
-    <div class="header-options">
-      <ul class="header-menu-info">
-        <li><a>intro</a></li>
-        <li><a>project</a></li>
-        <li><a>skill</a></li>
-      </ul>
+    <div class="header-container">
+      <div class="header-menu-flex">
+        <h1 class="header-logo">
+          <a @click="goToSomewhoere('')">Redd Lin</a>
+        </h1>
+      </div>
+      <div class="header-options">
+        <ul class="header-menu-info">
+          <li><a @click="goToSomewhoere('second')">intro</a></li>
+          <li><a>project</a></li>
+          <li><a>skill</a></li>
+        </ul>
+      </div>
+      <button :class="!mobile_list ? 'hamburger-icon' : 'hamburger-icon on'" @click="triggerMobileList">
+        <span class="icon-bar one" />
+        <span class="icon-bar two" />
+        <span class="icon-bar three" />
+      </button>
     </div>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      mobile_list: false
+    }
+  },
   methods: {
     goToSomewhoere(path) {
       this.$router.push('/Redd_Lin/' + path)
+    },
+    triggerMobileList() {
+      this.mobile_list = !this.mobile_list
     }
   }
 }
@@ -28,15 +43,21 @@ export default {
 @import "@/scss/theme";
 
 .header {
-    background: $color-bg-dark;
+  background: $color-bg-dark;
+  width: 100%;
+  top: 0;
+  position: fixed;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 0 30px;
+  .header-container {
+    max-width: 1200px;
     width: 100%;
-    top: 0;
-    position: fixed;
+    margin: auto;
     height: 80px;
     display: flex;
-    align-items: center;
-    box-sizing: border-box;
-    padding: 0 30px;
     .header-menu-flex {
       display: flex;
       -moz-box-align: center;
@@ -95,6 +116,51 @@ export default {
         }
       }
     }
+    @media only screen and (max-width: 650px) {
+      .header-options {
+        display: none;
+      }
+    }
+    button {
+      padding: 0;
+      background: none;
+      outline: none;
+    }
+    .hamburger-icon {
+      display: none;
+      border: none;
+      cursor: pointer;
+      .icon-bar {
+        background: $color-bg-icon-bar;
+        display: block;
+        height: 3px;
+        margin: 0 auto 6px;
+        width: 30px;
+        -webkit-transition: .3s ease-in-out;
+        transition: .3s ease-in-out;
+        border-radius: 10px;
+      }
+      &:hover .icon-bar {
+        background: $color-header-menu;
+      }
+    }
+    .hamburger-icon.on {
+      .one {
+        transform: rotate(45deg) translateX(6px) translateY(6px);
+      }
+      .two {
+        opacity: 0;
+      }
+      .three {
+        transform: rotate(-45deg) translateX(6px) translateY(-7px);
+      }
+    }
 
+    @media only screen and (max-width: 650px) {
+      .hamburger-icon {
+        display: block;
+      }
+    }
+  }
 }
 </style>
