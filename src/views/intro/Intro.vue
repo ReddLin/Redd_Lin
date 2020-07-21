4581c2e4da2e351344725edd07029c07d9a2c698
ReddLin <s9016030303@gmail.com>
2020-07-21 18:33:00 +0800

    start edit intro

--- src/views/intro/Intro.vue
+++ src/views/intro/Intro.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="intro">
-    <img src="https://images.cakeresume.com/s9016030303/48c17451-b8d8-4bef-b619-25acfdf35cfa.png" alt="">
+    <div class="first-block">
+      <img class="self-img" src="https://images.cakeresume.com/s9016030303/48c17451-b8d8-4bef-b619-25acfdf35cfa.png" alt="">
+      <div class="self-intro">
+        Redd Lin
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -13,6 +18,21 @@ export default {
 .intro {
   margin: auto 30px;
   color: $color-white;
-  
+  .first-block {
+    display: flex;
+    .self-img {
+      width: 50%;
+      max-width: 100%;
+      height: auto;
+      // display: flex;
+      text-align: center;
+      // flex: 1;
+      border-radius: 50%;
+    }
+    .self-intro {
+      // display: flex;
+      flex: 1;
+    }
+  }
 }
 </style>
