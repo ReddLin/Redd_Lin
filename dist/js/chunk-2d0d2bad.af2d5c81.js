7ff0f3d85d80c2df11e78406a68fd734c1267149
ReddLin <s9016030303@gmail.com>
2020-07-22 15:27:17 +0800

    update gitignore

--- dist/js/chunk-2d0d2bad.af2d5c81.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d2bad"],{"5a6a":function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"second"},[n._v(" Second~ ")])},c=[],s={},d=s,o=t("2877"),u=Object(o["a"])(d,a,c,!1,null,"77519df0",null);e["default"]=u.exports}}]);
-//# sourceMappingURL=chunk-2d0d2bad.af2d5c81.js.map
