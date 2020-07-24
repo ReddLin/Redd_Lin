05ba8a89138160be65c132a7c590a2bbf0ac24c3
ReddLin <s9016030303@gmail.com>
2020-07-24 14:57:43 +0800

    update about me

--- src/lang/tw.js
+++ src/lang/tw.js
@@ -2,5 +2,13 @@
 export default {
   __chineseName: '林明鴻',
   __englishName: 'Redd Lin',
-  __topIntro: '目前已有5年的前端網頁以及3年後端架構的經驗，主要以JAVA、nodeJs為網頁架構，配上HTML、Vue框架編輯前端頁面，以及開發功能系統後台、撰寫前端或是外部使用的API接口。'
+  __topIntro: '目前已有5年的前端網頁以及3年後端架構的經驗，主要以JAVA、nodeJs為網頁架構，配上HTML、Vue框架編輯前端頁面，以及開發功能系統後台、撰寫前端或是外部使用的API接口。',
+  __aboutMe: 'About Me',
+  __jobName: '前端工程師',
+  __education: 'EDUCATION:',
+  __educationTime: '2012 年 9 月 - 2014 年 6 月',
+  __educationTitle: '中原大學資訊工程學系碩士 畢',
+  __address: 'New Taipei City，TW',
+  __mail: 'Mail：',
+  __phone: 'Phone：'
 }
--- src/views/intro/Intro.vue
+++ src/views/intro/Intro.vue
@@ -9,11 +9,36 @@
         <img src="@/assets/img/self-pic.jpg" alt="">
       </div>
     </div>
+    <div class="second-block">
+      <h1>{{ $t('__aboutMe') }}</h1>
+      <div class="aboutMe-content">
+        <p>{{ $t('__jobName') }}</p>
+        <br>
+        <h3>{{ $t('__education') }}</h3>
+        <p class="education-content">  {{ $t('__educationTime') }}</p>
+        <p class="education-content">  {{ $t('__educationTitle') }}</p>
+        <br>
+        <p>{{ $t('__address') }}</p>
+        <p>
+          {{ $t('__mail') }}
+          <a :href="'mailto:' + mail" class="intro-mail">{{ mail }}</a>
+        </p>
+        <p>
+          {{ $t('__phone') }}
+          <a :href="'tel:' + phone" class="intro-phone">+886-930-941-228</a>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
-
+  data() {
+    return {
+      mail: 's9016030303@gmail.com',
+      phone: '0930941228'
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -49,6 +74,38 @@ export default {
   @media only screen and (max-width: 650px) {
     .first-block {
       flex-direction: column;
+    }
+  }
+  .second-block {
+    width: 100%;
+    // height: 200px;
+    max-width: 1200px;
+    margin: 0 auto;
+    text-align: center;
+    background: $color-intro-second-block-bg;
+    padding-bottom: 20px;
+    h1 {
+      line-height: 150px;
+      margin: 0;
+    }
+    .aboutMe-content {
+      max-width: 900px;
+      text-align: left;
+      padding: 0 30px;
+      p, h3 {
+        margin: 0;
+      }
+      .education-content {
+        white-space: pre-wrap;
+      }
+      .intro-mail, .intro-phone {
+        text-decoration: none;
+        color: $color-header-menu;
+        &:hover {
+          color: $color-intro-mail-hover;
+          transition: .5s all;
+        }
+      }
     }
   }
 }
